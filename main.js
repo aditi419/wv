5baaf66f58f1b22813452161b6c3923dbd29df12
aditi419 <76972940+aditi419@users.noreply.github.com>
2023-04-23 13:37:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let latitude = 22.7868542, longitude = 88.3643296;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [longitude, latitude],
+    zoom: 4
+});
+
+var img1 = document.querySelector("#holocaust");
+var marker1 = new mapboxgl.Marker({
+    element: img1
+})
+.setLngLat([-80.136211, 25.795477])
+.addTo(map)
+
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
